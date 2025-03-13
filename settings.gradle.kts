bd4120271a61ec7f07a18631ed4d989455bf43f7
TakWolf <takwolf@foxmail.com>
2025-03-13 17:02:21 +0800

    migration to gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "bitsnpicas"
+
+include("bitsnpicas")
+include("mapedit")
+include("keyedit")
+include("unicode")
